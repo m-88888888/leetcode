192c8072b05defd7aa8a0f0c3a5a21c418c870aa
m-88888888 <mochi.h8888@gmail.com>
2022-11-17 23:47:22 +0900

    solve 14 (should retry)

---
+++ code/14.longest-common-prefix.rb
@@ -0,0 +1,17 @@
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+  return '' if strs.empty?
+
+  s = ''
+  base = strs[0]
+  for i in 0..base.length - 1
+    if strs.all? { |x| x[i] == base[i] }
+      s += base[i]
+    else
+      break
+    end
+  end
+  s
+end
+
